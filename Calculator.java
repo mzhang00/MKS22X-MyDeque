b1f468035c4d012fc6bd9c4907f8320e8318fefa
Michael Zhang <mzhang00@stuy.edu>
2019-03-27 21:26:28 -0400

    Update calculator

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,34 @@
+import java.util.*;
+import java.io.*;
+
 public class Calculator{
   public static double eval(String s){
-    return 0.0;
+    String operations = "+-*/%";
+    MyDeque<Integer> deq = new MyDeque<>();
+    String[] values = s.split(" ", -2);
+    for (int i = 0; i < values.length; i++){
+      if (operations.contains(values[i])){
+        if (values[i].equals("+")){  
+          deq.addFirst(deq.removeFirst() + deq.removeFirst());
+        }
+        if (values[i].equals("-")){  
+          deq.addFirst(deq.removeFirst() - deq.removeFirst());
+        }
+        if (values[i].equals("*")){  
+          deq.addFirst(deq.removeFirst() * deq.removeFirst());
+        }
+        if (values[i].equals("/")){  
+          deq.addFirst(deq.removeFirst() / deq.removeFirst());
+        }
+        deq.addFirst(deq.removeFirst() % deq.removeFirst());
+      }else{
+        deq.addFirst(Integer.parseInt(values[i]));
+      }
+    }
+    if (deq.size() == 1){
+      return deq.getFirst();
+    }else{
+      return -1;
+    }
   }
 }
