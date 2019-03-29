ef0c4e8f3c7ead2b654e6488265dcaef482cd53c
Michael Zhang <mzhang00@stuy.edu>
2019-03-28 23:44:35 -0400

    Test and update calculator

--- Calculator.java
+++ Calculator.java
@@ -4,27 +4,38 @@ import java.io.*;
 public class Calculator{
   public static double eval(String s){
     String operations = "+-*/%";
-    MyDeque<Integer> deq = new MyDeque<>();
+    MyDeque<Double> deq = new MyDeque<>();
+    double temp = 0.0;
     String[] values = s.split(" ", 0);
     for (int i = 0; i < values.length; i++){
       if (operations.contains(values[i])){
         if (values[i].equals("+")){  
-          deq.addFirst(deq.removeFirst() + deq.removeFirst());
+          temp = deq.removeFirst();
+          deq.addFirst(deq.removeFirst() + temp);
         }
-        if (values[i].equals("-")){  
-          deq.addFirst(deq.removeFirst() - deq.removeFirst());
+        if (values[i].equals("-")){
+          temp = deq.removeFirst();  
+          deq.addFirst(deq.removeFirst() - temp);
         }
-        if (values[i].equals("*")){  
-          deq.addFirst(deq.removeFirst() * deq.removeFirst());
+        if (values[i].equals("*")){
+          temp = deq.removeFirst();  
+          deq.addFirst(deq.removeFirst() * temp);
         }
-        if (values[i].equals("/")){  
-          deq.addFirst(deq.removeFirst() / deq.removeFirst());
+        if (values[i].equals("/")){
+          temp = deq.removeFirst();  
+          deq.addFirst(deq.removeFirst() / temp);
         }
-        deq.addFirst(deq.removeFirst() % deq.removeFirst());
+        if (values[i].equals("%")){
+          temp = deq.removeFirst();  
+          deq.addFirst(deq.removeFirst() % temp);
+        }
       }else{
-        deq.addFirst(Integer.parseInt(values[i]));
+        deq.addFirst(Double.parseDouble(values[i]));
       }
     }
     return deq.getFirst();    
+  }
+  public static void main(String[] args){
+    System.out.println("+-*/%".contains("1.0"));
   }
 }
